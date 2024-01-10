9a8f00b29fe6bd5b6cb0c9ea0cfa2e9b21c72edc
alejandro <alexgeorgegeer@gmail.com>
2024-01-10 12:34:17 -0600

    beginning phases of functionality

--- assets/js/alexapi.js
+++ assets/js/alexapi.js
@@ -1,7 +1,14 @@
 const iForm = document.querySelector('#ingredients');
 
-iForm.addEventListener('click', getCocktails);
+iForm.addEventListener('submit', getCocktails);
 
-function getCocktails() {
-    
+function getCocktails(e) {
+    e.preventDefault();
+
+    const userName = document.querySelector('#userName').value;
+    const ingredient1 = document.querySelector('#ingredient1').value;
+    const ingredient2 = document.querySelector('#ingredient2').value;
+    const ingredient3 = document.querySelector('#ingredient3').value;
+
+    console.log(userName, ingredient1, ingredient2, ingredient3);
 }
