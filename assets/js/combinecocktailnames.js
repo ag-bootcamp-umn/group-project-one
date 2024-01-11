8b0c108f9349650059149b43114b81555cb4183f
Jonathon Blom <blomjonathon@gmail.com>
2024-01-11 12:36:12 -0600

    deleted combinecocktailnames.js after adding code to script.js as a comment

--- assets/js/combinecocktailnames.js
+++
@@ -1,9 +0,0 @@
-const cocktails1 = ["a", "b", "c", "d"];
-const cocktails2 = ["a", "c", "d", "e"];
-const cocktails3 = ["a", "c", "d"];
-
-const finalDrinkArray = cocktails1.filter(item => {
-  return cocktails2.includes(item) && cocktails3.includes(item);
-});
-
-console.log(finalDrinkArray);
--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,23 @@ const finalDrinkArray = cocktails1.filter(item => {
 
 console.log(finalDrinkArray);
 
+
+// combine cocktailnames.js file
+
+// const cocktails1 = ["a", "b", "c", "d"];
+// const cocktails2 = ["a", "c", "d", "e"];
+// const cocktails3 = ["a", "c", "d"];
+
+// const finalDrinkArray = cocktails1.filter(item => {
+//   return cocktails2.includes(item) && cocktails3.includes(item);
+// });
+
+// console.log(finalDrinkArray);
+
+
+
+
+
 // Display them
 
 function displayCocktail(drinkId) {
