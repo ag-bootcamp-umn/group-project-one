534edec0c9c5f1546098fb858278888576ea2968
alejandro <alexgeorgegeer@gmail.com>
2024-01-11 01:33:00 -0600

    consolidated most of the code

--- assets/js/alexapi.js
+++ assets/js/alexapi.js
@@ -1,33 +1,55 @@
-const iForm = document.querySelector('#ingredients');
-const urlRoot = 'www.thecocktaildb.com/api/json/v1/1/filter.php?'
+const mainForm = document.querySelector("#mainForm");
+const searchResults = document.querySelector("#searchResults");
+const urlRoot = "www.thecocktaildb.com/api/json/v1/1/filter.php?";
 
-iForm.addEventListener('submit', getCocktails);
+mainForm.addEventListener("submit", getCocktails);
 
 function getCocktails(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const userName = document.querySelector('#userName').value;
-    const ingredient1 = document.querySelector('#ingredient1').value;
-    const ingredient2 = document.querySelector('#ingredient2').value;
-    const ingredient3 = document.querySelector('#ingredient3').value;
+  const userName = document.querySelector("#userName").value;
+  const ingredient1 = document.querySelector("#ingredient1").value;
+  const ingredient2 = document.querySelector("#ingredient2").value;
+  const ingredient3 = document.querySelector("#ingredient3").value;
 
-    console.log(userName, ingredient1, ingredient2, ingredient3);
+//   storeUserInput(userName);
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient1}&i=${ingredient2}&i=${ingredient3}`)
-        .then( function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            const drinks = data.drinks;
-        drinks.forEach( drink => {
-            const drName = drink.strDrink;
-            const drId = drink.idDrink;
-            const drThumb = drink.strDrinkThumb;
-            console.log(`
-            name: ${drName}
-            id: ${drId}
-            thumb: ${drThumb}
-            `)
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient1}&i=${ingredient2}&i=${ingredient3}`
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then( data => {
+      const drinks = data.drinks;
+      drinks.forEach((drink) => {
+        const {strDrink, idDrink, strDrinkThumb} = drink;
+
+        searchResults.innerHTML += `
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <img src="${strDrinkThumb}" class="card-img-top" alt="${strDrink}">
+                <div class="card-body">
+                    <h5 class="card-title">${strDrink}</h5>
+                    <button value="${idDrink}" class="drinkButton btn btn-warning">Select this drink</button>
+                </div>
+            </div>
+        </div>`;
+
+        searchResults.addEventListener('click', function(e) {
+            if (e.target.matches('.drinkButton')) {
+                console.log(e.target.value);
+                location.assign(`./finalalt.html?q=${e.target.value}&name=${userName}`);
+            } 
         });
-        })
+      });
+    });
 }
+
+
+function storeUserInput(userName) {
+    let drinksData = JSON.parse(localStorage.getItem('drinksData') || '[]');
+    const userData = [{userName}];
+    console.log(userData);
+}
+
