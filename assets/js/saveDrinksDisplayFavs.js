ccfa33cf096bd323c97be89cfc141317be0397f3
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-01-12 14:44:59 -0600

    added comments and fixed favorite variables based on simplified displayFavDrinks function

--- assets/js/saveDrinksDisplayFavs.js
+++ assets/js/saveDrinksDisplayFavs.js
@@ -1,47 +1,48 @@
-// store drink once Alex's saveDrink button is clicked
-// drinkID
-// const drinkID = however Alex stored it
-// display fav drinks once Alex's seeFavs button is clicked
 // pull the drinkID
+
+
 // display  userName, drinkID, drinkImage, drinkLink 
+function displayFavDrinks() {
+var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
-function displayDrink() {
-// Store the variables in localStorage
-// localStorage.setItem('userNames', JSON.stringify(userNames));
-// localStorage.setItem('drinkIDs', JSON.stringify(drinkIDs));
-// localStorage.setItem('drinkImages', JSON.stringify(drinkImages));
-// localStorage.setItem('drinkLinks', JSON.stringify(drinkLinks));
-
-// Retrieve the stored values from localStorage
-var storedUserNames = JSON.parse(localStorage.getItem('userNames')) || [];
-var storedDrinkIDs = JSON.parse(localStorage.getItem('drinkIDs')) || [];
-var storedDrinkImages = JSON.parse(localStorage.getItem('drinkImages')) || [];
-var storedDrinkLinks = JSON.parse(localStorage.getItem('drinkLinks')) || [];
-
+[
+    {
+        username: 'Katy',
+        drinkId: 1,
+        image: '...',
+        link: '...'
+    },
+    {
+        username: 'Dizzy',
+        drinkId: 2,
+        image: '...',
+        link: '...'
+    },
+]
     // Check if there are stored favorites
-    if (storedUserNames.length === 0) {
+    if (favorites.length === 0) {
         document.getElementById('favoritesContainer').textContent = 'There are no stored favorites';
         return;
     }
 
     // Create and append elements for each stored favorite
     var favoritesContainer = document.getElementById('favoritesContainer');
-    for (var i = 0; i < storedUserNames.length; i++) {
+    for (var i = 0; i < favorites.length; i++) {
         var favoriteDiv = document.createElement('div');
         favoriteDiv.classList.add('favorite');
 
         var userNameP = document.createElement('p');
-        userNameP.textContent = 'User Name: ' + storedUserNames[i];
+        userNameP.textContent = 'User Name: ' + favorites[i].username;
 
         var drinkIDP = document.createElement('p');
-        drinkIDP.textContent = 'Drink ID: ' + storedDrinkIDs[i];
+        drinkIDP.textContent = 'Drink ID: ' + favorites[i].drinkId;
 
         var drinkImage = document.createElement('img');
-        drinkImage.src = storedDrinkImages[i];
+        drinkImage.src = favorites[i].image;
         drinkImage.alt = 'Drink Image';
 
         var drinkLink = document.createElement('a');
-        drinkLink.href = storedDrinkLinks[i];
+        drinkLink.href = favorites[i].link;
         drinkLink.target = '_blank';
         drinkLink.textContent = 'Drink Link';
 
